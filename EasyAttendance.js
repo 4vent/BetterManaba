af853481259cdbd874e858be31b9f34333e58b0d
4vent <abeckento@gmail.com>
2022-10-19 12:38:55 +0900

    EasyAttendance

--- EasyAttendance.js
+++ EasyAttendance.js
@@ -1,18 +1,31 @@
 function getTextBlockHTML() {
     return `
-    <div class="my-infolist my-infolist-searchall">
-        <div class="my-infolist-header">
-            <h2>出席カード</h2>
+        <div class="my-infolist my-infolist-searchall">
+            <div class="my-infolist-header">
+                <h2>出席カード</h2>
+            </div>
+            <div class="my-infolist-body"><div style="padding:10px; text-align:right" class="newssearch-box">
+                <input type="text" name="attendance" class="editable" placeholder="出席カード番号" style="width: 140px;">
+                <input type="button" name="attendance" value="✈️" style="height: 20px;" onclick="onClickEasyAttendanceButton();">
+            </div></div>
         </div>
-        <div class="my-infolist-body"><div style="padding:10px; text-align:right" class="newssearch-box"><table><tbody>
-            <input type="text" name="attendance">
-        </tbody></table></div></div>
-    </div>
     `
 }
 
+/** @type {HTMLInputElement} */
+var easyAttendanceInputField;
+
 function main() {
-    const 
+    const div = document.createElement("div");
+    div.innerHTML = getTextBlockHTML();
+    const right_body = document.getElementsByClassName("contentbody-right")[0];
+    right_body.insertBefore(div, right_body.children[0]);
+    easyAttendanceInputField = div.getElementsByClassName("editable")[0]
+}
+
+const onClickEasyAttendanceButton = () => {
+    const num = easyAttendanceInputField.value;
+    window.open("https://ctat.ritsumei.ac.jp/attend/ctat?lang=ja&code=" + num);
 }
 
 // if (document.URL == "https://ct.ritsumei.ac.jp/ct/home" || document.URL == "https://ct.ritsumei.ac.jp/ct/home_course") {
