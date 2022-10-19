9a19e98aec0a2d4844bd0376adbcf47519a8de63
4vent <abeckento@gmail.com>
2022-10-19 13:41:38 +0900

    Deadline Style change

--- DeadlineList.js
+++ DeadlineList.js
@@ -1,4 +1,5 @@
-const _1MIN = 60 * 1000;
+const _1SEC = 1000;
+const _1MIN = 60 * _1SEC;
 const _1HUR = 60 * _1MIN;
 const _1DAY = 24 * _1HUR;
 const _1MON = 30 * _1DAY;
@@ -44,7 +45,7 @@ function getManabaTableHTML(title) {
 function getManabaTableRowHTML(dateColor, textColor, isBold, expredTxt, title, title_link, course, cource_link) {
     return `
         <td style="width: 60px">
-            <div class="news-courseinfo" style="width: 60px; color: #${dateColor}; ${(isBold) ? 'font-weight: bold;' : ''}">
+            <div class="news-courseinfo" style="width: 70px; color: #${dateColor}; font-family: monospace;${(isBold) ? 'font-weight: bold;' : ''}">
                 ${expredTxt}
             </div>
         </td>
@@ -120,6 +121,49 @@ function genManabaTable(title) {
     return div.children[0].children[3].children[0].children[0].children[0]
 }
 
+/** @param {int} value @param {int} dig */
+function zfill(value, dig) {
+    const txt = String(value)
+    if (txt.length > dig) return txt;
+    else return (Array(dig).join('0') + value).slice(-dig);
+}
+/** @param {int} value @param {int} dig */
+function sfill(value, dig) {
+    const txt = String(value)
+    if (txt.length > dig) return txt;
+    else return (Array(dig).join('0') + value).slice(-dig);
+}
+
+/** @param {HTMLTableElement} tbody */
+function updateDeadline(tbody, marged_asgmts) {
+    Array.from(tbody.children).forEach(c => tbody.removeChild(c));
+    Object.values(marged_asgmts).forEach(asgmt => {
+        const due = asgmt.due
+        const now = new Date().getTime()
+        const expredIn = due - now
+        const expredTxt = (expredIn < _1DAY)? "残り" + zfill(Math.floor(expredIn / _1HUR) % 24, 2) + ":"
+                                                     + zfill(Math.floor(expredIn / _1MIN) % 60, 2) + ":"
+                                                     + zfill(Math.floor(expredIn / _1SEC) % 60, 2) : 
+                          (expredIn < EXPIRE_WARN)? String(Math.floor(expredIn / _1DAY) % 30) + "日と"
+                                                    + String(Math.floor(expredIn / _1HUR) % 24) + "時間" :
+                          (expredIn < _1MON)?       String(Math.floor(expredIn / _1DAY) % 30) + "日後" :
+                                                    String(Math.floor(expredIn / _1MON)) + "ヶ月後"
+        var bgcolor, dateColor, textColor;
+        if      (expredIn < EXPIRE_CRISIS) {bgcolor = "000000"; dateColor = "ef857d"; textColor = "ef857d";}
+        else if (expredIn < EXPIRE_DANGER) {bgcolor = "ee99ff"; dateColor = "47266e";}
+        else if (expredIn < EXPIRE_RISKY ) {bgcolor = "f19ca7"; dateColor = "6c272d";}
+        else if (expredIn < EXPIRE_WARN  ) {bgcolor = "ffe9a9"; dateColor = "866629";}
+        else if (expredIn < EXPIRE_INFO  ) {bgcolor = "a3e6e6"; dateColor = "006948";}
+        const tr = document.createElement("tr")
+        tr.setAttribute("style", "background-color: #" + bgcolor);
+        tr.innerHTML = getManabaTableRowHTML(dateColor, textColor, (expredIn < 5 * 24 * 3600000), expredTxt,
+                                            asgmt.title, asgmt.title_link, asgmt.cource, asgmt.cource_link)
+
+        tbody.appendChild(tr);
+        setTimeout
+    });
+}
+
 async function main() {
     const asgmts = await getManabaAsgments()
     const tbody = genManabaTable('未提出課題')
@@ -130,28 +174,9 @@ async function main() {
     let result = Object.keys(marged_asgmts)
         .map(key => marged_asgmts[key])
         .sort((a, b) => (a.due < b.due) ? -1 : 1);
-
-    Object.values(result).forEach(asgmt => {
-        const due = asgmt.due
-        const now = new Date().getTime()
-        const expredIn = due - now
-        const expredTxt = (expredIn < _1HUR) ? String(Math.floor(expredIn / _1MIN)) + "分後" :
-                          (expredIn < _1DAY) ? String(Math.floor(expredIn / _1HUR)) + "時間後" :
-                          (expredIn < _1MON) ? String(Math.floor(expredIn / _1DAY)) + "日後" :
-                                               String(Math.floor(expredIn / _1MON)) + "ヶ月後";
-        var bgcolor, dateColor, textColor;
-        if      (expredIn < EXPIRE_CRISIS) {bgcolor = "000000"; dateColor = "ef857d"; textColor = "ef857d";}
-        else if (expredIn < EXPIRE_DANGER) {bgcolor = "ee99ff"; dateColor = "47266e";}
-        else if (expredIn < EXPIRE_RISKY ) {bgcolor = "f19ca7"; dateColor = "6c272d";}
-        else if (expredIn < EXPIRE_WARN  ) {bgcolor = "ffe9a9"; dateColor = "866629";}
-        else if (expredIn < EXPIRE_INFO  ) {bgcolor = "a3e6e6"; dateColor = "006948";}
-        const tr = document.createElement("tr")
-        tr.setAttribute("style", "background-color: #" + bgcolor);
-        tr.innerHTML = getManabaTableRowHTML(dateColor, textColor, (expredIn < 5 * 24 * 3600000), expredTxt,
-                                             asgmt.title, asgmt.title_link, asgmt.cource, asgmt.cource_link)
-
-        tbody.appendChild(tr);
-    })
+    
+    updateDeadline(tbody, result);
+    setInterval(() => updateDeadline(tbody, result), 1000);
 }
 
 if (document.URL == "https://ct.ritsumei.ac.jp/ct/home" || document.URL == "https://ct.ritsumei.ac.jp/ct/home_course") {
